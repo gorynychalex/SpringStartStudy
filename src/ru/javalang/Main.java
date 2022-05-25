d5dec92afd57f693807370fc50f3080d79fc6066
gorynychalex <gorynychalex@gmail.com>
2022-05-25 12:38:21 +1000

    change message but leave Hello

--- src/ru/javalang/Main.java
+++ src/ru/javalang/Main.java
@@ -2,6 +2,8 @@ package ru.javalang;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет!");
+        if(args.length > 0) {
+            for(String s: args) System.out.println(s);
+        } else System.out.println("Привет!");
     }
 }
